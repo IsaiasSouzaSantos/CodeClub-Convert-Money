cd5b201cf66ae3b4f340c38f1f577dbfa61cf426
isaias souza santos <isaiassouza467@gmail.com>
2022-09-09 00:45:23 -0300

    consumindo API AWESOMEADI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
 const button = document.getElementById('convert-button');
 const select = document.getElementById("currency-select");
 
-const dolar = 5.39;
-const euro = 5.40;
-const bitcoin = 109.15781;
-
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-real').value;
     const realValueText = document.getElementById("real-value-text");
     const currencyValueText = document.getElementById("currency-value-text");
 
     //realValueText.innerHTML = inputReais;
+
+   const data =  await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+   
+   const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+
+  // console.log(data)
 
     realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
